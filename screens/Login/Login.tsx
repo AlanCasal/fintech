be73254e8f161049a469b19ca463e7c6b27a4a2a
Alan Casal <alan.casal.dev@gmail.com>
2025-01-01 16:19:54 -0300

    added Signin screen UI

--- screens/Login/Login.tsx
+++ screens/Login/Login.tsx
@@ -1,12 +1,150 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import {
+	View,
+	Text,
+	TextInput,
+	TouchableOpacity,
+	KeyboardAvoidingView,
+	Platform,
+} from 'react-native';
+import React, { useState } from 'react';
+import { defaultStyles } from '@/constants/Styles';
 import { styles } from './styles';
+import Colors from '@/constants/Colors';
+import { Link, Stack, useRouter } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
+
+enum SignInType {
+	PHONE = 'phone',
+	EMAIL = 'email',
+	GOOGLE = 'google',
+	APPLE = 'apple',
+}
+
+const CONTINUE_WITH_BUTTONS = [
+	{
+		type: SignInType.EMAIL,
+		icon: 'mail',
+		text: 'Continue with Email',
+	},
+	{
+		type: SignInType.GOOGLE,
+		icon: 'logo-google',
+		text: 'Continue with Google',
+	},
+	{
+		type: SignInType.APPLE,
+		icon: 'logo-apple',
+		text: 'Continue with Apple',
+	},
+];
 
 const Login = () => {
+	const [countryCode, setCountryCode] = useState('+49');
+	const [mobileNumber, setMobileNumber] = useState('');
+	const router = useRouter();
+	const keyboardVerticalOffset = Platform.OS === 'ios' ? 80 : 0;
+
+	const handleLogin = async (type: SignInType) => {
+		console.log('%c[handleLogin]', 'background: #000059; color: #9fcfff', type);
+
+		if (type === SignInType.PHONE) {
+			// ...
+		}
+	};
+
 	return (
-		<View style={styles.container}>
-			<Text>Login</Text>
-		</View>
+		<KeyboardAvoidingView
+			behavior="padding"
+			style={{ flex: 1 }}
+			keyboardVerticalOffset={keyboardVerticalOffset}
+		>
+			<Stack.Screen
+				name="login"
+				options={{
+					title: '',
+					headerBackTitle: '',
+					headerShadowVisible: false,
+					headerStyle: {
+						backgroundColor: Colors.background,
+					},
+					headerLeft: () => (
+						<TouchableOpacity onPress={router.back}>
+							<Ionicons name="arrow-back" size={34} color={Colors.dark} />
+						</TouchableOpacity>
+					),
+					headerRight: () => (
+						<Link href="/help" asChild>
+							<TouchableOpacity>
+								<Ionicons
+									name="help-circle-outline"
+									size={34}
+									color={Colors.dark}
+								/>
+							</TouchableOpacity>
+						</Link>
+					),
+				}}
+			/>
+
+			<View style={[defaultStyles.container, styles.container]}>
+				<Text style={defaultStyles.header}>Welcome Back !</Text>
+				<Text style={defaultStyles.descriptionText}>
+					Enter the phone number associated with your account
+				</Text>
+
+				<View style={styles.inputContainer}>
+					<TextInput
+						style={styles.input}
+						placeholderTextColor={Colors.gray}
+						keyboardType="numeric"
+						value={countryCode}
+					/>
+					<TextInput
+						style={[styles.input, styles.inputMobileNumber]}
+						placeholder="Mobile number"
+						placeholderTextColor={Colors.gray}
+						keyboardType="numeric"
+						value={mobileNumber}
+						onChangeText={setMobileNumber}
+					/>
+				</View>
+
+				<TouchableOpacity
+					style={[
+						defaultStyles.pillButton,
+						styles.loginButton,
+						!mobileNumber ? styles.loginDisabled : styles.loginEnabled,
+					]}
+					onPress={() => handleLogin(SignInType.PHONE)}
+					disabled={!mobileNumber}
+				>
+					<Text style={defaultStyles.buttonText}>Continue</Text>
+				</TouchableOpacity>
+
+				<View style={styles.dividerContainer}>
+					<View style={styles.divider} />
+					<Text style={defaultStyles.descriptionText}>or</Text>
+					<View style={styles.divider} />
+				</View>
+
+				{CONTINUE_WITH_BUTTONS.map(button => (
+					<TouchableOpacity
+						key={button.type}
+						style={[defaultStyles.pillButton, styles.continueWithButton]}
+						onPress={() => handleLogin(button.type)}
+					>
+						<Ionicons
+							name={button.icon as keyof typeof Ionicons.glyphMap}
+							size={24}
+							color={Colors.dark}
+						/>
+						<Text style={[defaultStyles.buttonText, styles.continueWithText]}>
+							{button.text}
+						</Text>
+					</TouchableOpacity>
+				))}
+			</View>
+		</KeyboardAvoidingView>
 	);
 };
 
--- screens/Login/styles.ts
+++ screens/Login/styles.ts
@@ -1,8 +1,48 @@
+import Colors from '@/constants/Colors';
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
 	container: {
+		gap: 20,
+	},
+	inputContainer: {
+		flexDirection: 'row',
+		gap: 10,
+	},
+	input: {
+		backgroundColor: Colors.lightGray,
+		padding: 20,
+		borderRadius: 16,
+		fontSize: 20,
+	},
+	inputMobileNumber: {
 		flex: 1,
-		backgroundColor: 'red',
+	},
+	loginButton: {
+		marginBottom: 20,
+	},
+	loginEnabled: {
+		backgroundColor: Colors.dark,
+	},
+	loginDisabled: {
+		backgroundColor: Colors.primaryMuted,
+	},
+	dividerContainer: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		gap: 16,
+	},
+	divider: {
+		flex: 1,
+		height: StyleSheet.hairlineWidth,
+		backgroundColor: Colors.gray,
+	},
+	continueWithButton: {
+		flexDirection: 'row',
+		gap: 10,
+		backgroundColor: 'white',
+	},
+	continueWithText: {
+		color: Colors.dark,
 	},
 });
