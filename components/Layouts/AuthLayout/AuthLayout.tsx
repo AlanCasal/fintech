d82015c39e45dafbe2ba16649fbc5c586ef8d40f
Alan Casal <alan.casal.dev@gmail.com>
2025-01-02 14:46:01 -0300

    added blur to tabs and moved auth layout

---
+++ components/Layouts/AuthLayout/AuthLayout.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { Stack, Tabs } from 'expo-router';
+import Colors from '@/constants/Colors';
+import { FontAwesome } from '@expo/vector-icons';
+import { BlurView } from 'expo-blur';
+import { styles } from './styles';
+
+const AuthLayout = () => {
+	return (
+		<>
+			<Stack.Screen
+				name="(authenticated)/(tabs)"
+				options={{ headerShown: false }}
+			/>
+			<Tabs
+				screenOptions={{
+					tabBarActiveTintColor: Colors.primary,
+					tabBarBackground: () => (
+						<BlurView intensity={100} style={styles.blurView} />
+					),
+					tabBarStyle: styles.tabBarStyle,
+				}}
+			>
+				<Tabs.Screen
+					name="home"
+					options={{
+						title: 'Home',
+						tabBarIcon: ({ color, size }) => (
+							<FontAwesome name="home" size={size} color={color} />
+						),
+					}}
+				/>
+				<Tabs.Screen
+					name="invest"
+					options={{
+						title: 'Invest',
+						tabBarIcon: ({ color, size }) => (
+							<FontAwesome name="line-chart" size={size} color={color} />
+						),
+					}}
+				/>
+				<Tabs.Screen
+					name="transfers"
+					options={{
+						title: 'Transfers',
+						tabBarIcon: ({ color, size }) => (
+							<FontAwesome name="exchange" size={size} color={color} />
+						),
+					}}
+				/>
+				<Tabs.Screen
+					name="crypto"
+					options={{
+						title: 'Crypto',
+						tabBarIcon: ({ color, size }) => (
+							<FontAwesome name="bitcoin" size={size} color={color} />
+						),
+					}}
+				/>
+				<Tabs.Screen
+					name="lifestyle"
+					options={{
+						title: 'Lifestyle',
+						tabBarIcon: ({ color, size }) => (
+							<FontAwesome name="th" size={size} color={color} />
+						),
+					}}
+				/>
+			</Tabs>
+		</>
+	);
+};
+
+export default AuthLayout;
---
+++ components/Layouts/AuthLayout/styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+	blurView: {
+		flex: 1,
+		backgroundColor: 'rgba(255, 255, 255, 0.05)',
+	},
+	tabBarStyle: {
+		backgroundColor: 'transparent',
+		position: 'absolute',
+		bottom: 0,
+		left: 0,
+		right: 0,
+		borderTopWidth: 0,
+		elevation: 0,
+	},
+});
