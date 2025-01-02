2338556b942cbd7c74ce138fa632dcb0faf242ee
Alan Casal <alan.casal.dev@gmail.com>
2025-01-02 20:23:07 -0300

    added latest Crypto detail UI

--- screens/Crypto/Crypto.tsx
+++ screens/Crypto/Crypto.tsx
@@ -1,12 +1,16 @@
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, TouchableOpacity, ScrollView } from 'react-native';
 import React from 'react';
 import { defaultStyles } from '@/constants/Styles';
 import { useQuery } from '@tanstack/react-query';
 import { CurrencyData } from '@/interfaces/crypto';
 import { styles } from './styles';
 import LoadingSpinner from '@/components/LoadingSpinner';
+import { Link } from 'expo-router';
+import { useHeaderHeight } from '@react-navigation/elements';
+import { Ionicons } from '@expo/vector-icons';
 
 const Crypto = () => {
+	const headerHeight = useHeaderHeight();
 	const {
 		data: currencies,
 		isLoading: currenciesLoading,
@@ -48,18 +52,56 @@ const Crypto = () => {
 		);
 
 	return (
-		<View style={defaultStyles.container}>
-			{!!currencies &&
-				currencies.map((currency: CurrencyData) => (
-					<View style={styles.currencyWrapper} key={currency.id}>
-						<Image
-							source={{ uri: currenciesData?.[currency.id]?.logo }}
-							style={styles.currencyImage}
-						/>
-						<Text>{currency.name}</Text>
-					</View>
-				))}
-		</View>
+		<ScrollView
+			style={styles.container}
+			contentContainerStyle={{ paddingTop: headerHeight }}
+		>
+			<Text style={defaultStyles.sectionHeader}>Latest Crypto</Text>
+
+			<View style={defaultStyles.block}>
+				{!!currencies &&
+					currencies.map((currency: CurrencyData) => {
+						const { percent_change_1h: percentChange, price } =
+							currency.quote.EUR;
+						const isChangePositive = percentChange > 0;
+
+						return (
+							<Link href={`/crypto/${currency.id}`} key={currency.id} asChild>
+								<TouchableOpacity style={styles.currencyWrapper}>
+									<Image
+										source={{ uri: currenciesData?.[currency.id]?.logo }}
+										style={styles.currencyImage}
+									/>
+									<View style={styles.currencyInfoWrapper}>
+										<Text style={styles.currencyName}>{currency.name}</Text>
+										<Text style={styles.currencySymbol}>{currency.symbol}</Text>
+									</View>
+
+									<View style={styles.currencyPriceWrapper}>
+										<Text style={styles.currencyPrice}>
+											{price.toFixed(2)} €
+										</Text>
+										<View style={styles.priceChangeWraper}>
+											<Ionicons
+												name={isChangePositive ? 'caret-up' : 'caret-down'}
+												size={16}
+												color={isChangePositive ? 'green' : 'red'}
+											/>
+											<Text
+												style={{
+													color: isChangePositive ? 'green' : 'red',
+												}}
+											>
+												{percentChange.toFixed(2)} %
+											</Text>
+										</View>
+									</View>
+								</TouchableOpacity>
+							</Link>
+						);
+					})}
+			</View>
+		</ScrollView>
 	);
 };
 
--- screens/Crypto/styles.ts
+++ screens/Crypto/styles.ts
@@ -1,11 +1,42 @@
+import Colors from '@/constants/Colors';
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: Colors.background,
+	},
 	currencyWrapper: {
 		flexDirection: 'row',
+		alignItems: 'center',
+		gap: 10,
 	},
 	currencyImage: {
-		width: 32,
-		height: 32,
+		width: 40,
+		height: 40,
+	},
+	currencyInfoWrapper: {
+		flex: 1,
+		gap: 4,
+	},
+	currencyName: {
+		fontWeight: '600',
+		color: Colors.dark,
+	},
+	currencySymbol: {
+		color: Colors.gray,
+	},
+	currencyPriceWrapper: {
+		gap: 4,
+		alignItems: 'flex-end',
+	},
+	currencyPrice: {
+		fontWeight: '600',
+		color: Colors.dark,
+	},
+	priceChangeWraper: {
+		flexDirection: 'row',
+		gap: 4,
+		alignItems: 'center',
 	},
 });
