ad4d558c260e09596267e8469663bd22e1bcba7d
Alan Casal <alan.casal.dev@gmail.com>
2025-01-02 22:52:08 -0300

    added btns and sticky header

--- screens/CryptoDetail/CryptoDetail.tsx
+++ screens/CryptoDetail/CryptoDetail.tsx
@@ -1,6 +1,6 @@
 import { Ionicons } from '@expo/vector-icons';
 import { Stack, useLocalSearchParams, useRouter } from 'expo-router';
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, SectionList, Image } from 'react-native';
 import Colors from '@/constants/Colors';
 import { styles } from './styles';
@@ -8,10 +8,17 @@ import { defaultStyles } from '@/constants/Styles';
 import { useQuery } from '@tanstack/react-query';
 import { CryptoInfo } from '@/interfaces/crypto';
 import LoadingSpinner from '@/components/LoadingSpinner';
+import { useHeaderHeight } from '@react-navigation/elements';
+import { ScrollView } from 'react-native-gesture-handler';
+
+const CATEGORIES = ['Overview', 'News', 'Orders', 'Transactions'];
 
 const Crypto = () => {
+	const [activeIndex, setActiveIndex] = useState(0);
+
 	const { id } = useLocalSearchParams();
 	const router = useRouter();
+	const headerHeight = useHeaderHeight();
 
 	const { data, isLoading, error } = useQuery<CryptoInfo, Error>({
 		queryKey: ['info', id],
@@ -55,6 +62,7 @@ const Crypto = () => {
 				}}
 			/>
 			<SectionList
+				style={{ marginTop: headerHeight }}
 				contentInsetAdjustmentBehavior="automatic"
 				keyExtractor={(item, index) => item.title + index}
 				sections={[{ data: [{ title: 'Section 1' }] }]}
@@ -67,9 +75,67 @@ const Crypto = () => {
 								style={styles.headerComponentLogo}
 							/>
 						</View>
+
+						<View style={styles.buttonsWrapper}>
+							<TouchableOpacity
+								style={[defaultStyles.pillButtonSmall, styles.pillButton]}
+							>
+								<Ionicons name="add" size={24} color="white" />
+								<Text style={[defaultStyles.buttonText, styles.buttonText]}>
+									Buy
+								</Text>
+							</TouchableOpacity>
+							<TouchableOpacity
+								style={[
+									defaultStyles.pillButtonSmall,
+									styles.pillButton,
+									{ backgroundColor: Colors.primaryMuted },
+								]}
+							>
+								<Ionicons name="arrow-back" size={24} color={Colors.primary} />
+								<Text
+									style={[
+										defaultStyles.buttonText,
+										styles.buttonText,
+										{ color: Colors.primary },
+									]}
+								>
+									Receive
+								</Text>
+							</TouchableOpacity>
+						</View>
 					</>
 				)}
-				renderItem={({ item }) => (
+				renderSectionHeader={() => (
+					<ScrollView
+						horizontal
+						showsHorizontalScrollIndicator={false}
+						contentContainerStyle={styles.headerCategoriesWrapper}
+					>
+						{CATEGORIES.map((category, index) => (
+							<TouchableOpacity
+								onPress={() => setActiveIndex(index)}
+								key={category}
+								style={
+									index === activeIndex
+										? styles.categoriesButtonActive
+										: styles.categoriesButton
+								}
+							>
+								<Text
+									style={
+										index === activeIndex
+											? styles.categoryTextActive
+											: styles.categoryText
+									}
+								>
+									{category}
+								</Text>
+							</TouchableOpacity>
+						))}
+					</ScrollView>
+				)}
+				renderItem={() => (
 					<>
 						<View style={[defaultStyles.block, styles.overview]}>
 							<Text style={styles.subtitle}>Overview</Text>
--- screens/CryptoDetail/styles.ts
+++ screens/CryptoDetail/styles.ts
@@ -6,6 +6,11 @@ export const styles = StyleSheet.create({
 		flexDirection: 'row',
 		gap: 10,
 	},
+	stickyHeaderComponentWrapper: {
+		backgroundColor: 'red',
+		height: 50,
+		flex: 1,
+	},
 	headerComponentWrapper: {
 		flexDirection: 'row',
 		justifyContent: 'space-between',
@@ -21,6 +26,53 @@ export const styles = StyleSheet.create({
 	headerComponentLogo: {
 		width: 60,
 		height: 60,
+	},
+	buttonsWrapper: {
+		flexDirection: 'row',
+		gap: 10,
+		margin: 12,
+	},
+	pillButton: {
+		backgroundColor: Colors.primary,
+		flexDirection: 'row',
+		gap: 16,
+	},
+	buttonText: {
+		color: 'white',
+	},
+	headerCategoriesWrapper: {
+		width: '100%',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+		paddingHorizontal: 16,
+		gap: 10,
+		paddingBottom: 8,
+		backgroundColor: Colors.background,
+		borderBottomColor: Colors.gray,
+		borderBottomWidth: StyleSheet.hairlineWidth,
+	},
+	categoriesButton: {
+		padding: 10,
+		paddingHorizontal: 14,
+		alignItems: 'center',
+		justifyContent: 'center',
+		borderRadius: 20,
+	},
+	categoriesButtonActive: {
+		padding: 10,
+		paddingHorizontal: 14,
+		alignItems: 'center',
+		justifyContent: 'center',
+		borderRadius: 20,
+		backgroundColor: 'white',
+	},
+	categoryText: {
+		fontSize: 14,
+		color: Colors.gray,
+	},
+	categoryTextActive: {
+		fontSize: 14,
+		color: Colors.dark,
 	},
 	overview: {
 		marginTop: 20,
